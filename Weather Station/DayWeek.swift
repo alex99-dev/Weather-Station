fd3c159b3eb3c78de1f74f1d8a7213ae628e41a8
alex99-dev <buliga.alexandru99@gmail.com>
2021-03-21 18:00:22 +0200

    Weather-Station Release V1.0

---
+++ Weather Station/DayWeek.swift
@@ -0,0 +1,8 @@
+//
+//  DayWeek.swift
+//  Weather Station
+//
+//  Created by Buliga Alexandru on 21.03.2021.
+//
+
+import Foundation
---
+++ Weather Station/weatherCell.swift
@@ -0,0 +1,13 @@
+//
+//  customCell.swift
+//  Weather Station
+//
+//  Created by Buliga Alexandru on 15.03.2021.
+//
+
+import Foundation
+import UIKit
+
+class weatherCell : UITableViewCell{
+    
+}
